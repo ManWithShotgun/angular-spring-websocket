98fa13cfbc649d4d241a459998aa3373e1ec1f0d
ManWithShotGun <u3n3ter7@gmail.com>
2019-04-03 21:27:50 +0300

    added websocket support

---
+++ tutorial-web/src/main/web/src/app/websoket.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsoketService {
+  private serverUrl = 'http://localhost:8090/socket'
+  private title = 'WebSockets chat';
+  private stompClient;
+  constructor() { }
+
+  initializeWebSocketConnection(): void {
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          // $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message): void {
+    this.stompClient.send("/app/send/message" , {}, message);
+    // $('#input').val('');
+  }
+}
