13a22f6b2985b3221e36e2ce19b286a25d2af065
Ilya_Burym <ilya.burym@wolterskluwer.com>
2019-04-11 12:15:54 +0300

    added ws stats print configuration

--- src/main/java/ru/ilia/app/configuration/WebSocketConfiguration.java
+++ src/main/java/ru/ilia/app/configuration/WebSocketConfiguration.java
@@ -1,14 +1,27 @@
 package ru.ilia.app.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private WebSocketMessageBrokerStats webSocketMessageBrokerStats;
+
+    @PostConstruct
+    public void init() {
+        webSocketMessageBrokerStats.setLoggingPeriod(600 * 1000);
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
