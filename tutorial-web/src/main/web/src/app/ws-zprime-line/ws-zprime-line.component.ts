f2608aedeb983f1dca87c1bcc9e9481143dae598
ManWithShotGun <u3n3ter7@gmail.com>
2019-05-01 00:04:15 +0300

    added validation line control

--- tutorial-web/src/main/web/src/app/ws-zprime-line/ws-zprime-line.component.ts
+++ tutorial-web/src/main/web/src/app/ws-zprime-line/ws-zprime-line.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { MainGraphService } from '../graph/main-graph.service';
 
 @Component({
@@ -10,13 +10,25 @@ import { MainGraphService } from '../graph/main-graph.service';
 export class WsZprimeLineComponent implements OnInit {
 
   requestData = this.fb.group({
-    ksi: [null, Validators.required],
+    ksi: [null, [Validators.required, this.isDouble]],
   });
 
   constructor(private fb: FormBuilder, public mainGraphService: MainGraphService) { }
 
   ngOnInit() {
     this.mainGraphService.getLinesInfo();
+  }
+
+  public isDouble(control: AbstractControl): ValidationErrors | null {
+    let value = Number.parseFloat(control.value);
+    if (Number.isNaN(value)) {
+      return {isNotDouble:'It is not valid double value'};
+    }
+    return null;
+  }
+
+  public requestLine() {
+    this.mainGraphService.requestLine(this.requestData.controls.ksi.value);
   }
 
 }
